585edbd870967d5d8032db362788bac4baf1bdf6
LegoFigure11 <legofigurefb@gmail.com>
2017-04-15 11:24:10 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var runtest = function()
 {
 	//first, to parse it all from the PS format
 	var string = document.getElementById('customMon').value
-	var spreadName = document.getElementById('spreadName').value
-	if(spreadName == '')
-		spreadName = "My Custom Set";
 	var lines = string.split('\n')
 	var species = "";
 	var item = "";
