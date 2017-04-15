3c541261c64ef84e4b29020d6178fdd5a80abf42
LegoFigure11 <legofigurefb@gmail.com>
2017-04-15 11:25:24 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+var showdownFormes = [["Kyurem-White", "Kyurem-W"],
+["Kyurem-Black", "Kyurem-B"],
+["Rotom-Wash", "Rotom-W"],
+["Rotom-Heat", "Rotom-H"],
+["Rotom-Frost", "Rotom-F"],
+["Rotom-Mow", "Rotom-C"],
+["Rotom-Fan", "Rotom-S"],
+["Giratina-Origin", "Giratina-O"],
+["Landorus-Therian", "Landorus-T"],
+["Thundurus-Therian", "Thundurus-T"],
+["Tornadus-Therian", "Tornadus-T"],
+["Floette-Eternal", "Floette-E"],
+["Pumpkaboo", "Pumpkaboo-Average"],
+["Gourgeist", "Gourgeist-Average"],
+["Wormadan-Sandy", "Wormadan-G"],
+["Wormadan-Trash", "Wormadan-S"],
+["Groudon-Primal", "Groudon"],
+["Kyogre-Primal", "Kyogre"]];
+
 var runtest = function()
 {
 	//first, to parse it all from the PS format
