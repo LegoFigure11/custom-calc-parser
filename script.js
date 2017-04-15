ab27411a2a80af2ccde1d2af96e78c4497b5d251
LegoFigure11 <legofigurefb@gmail.com>
2017-04-15 22:10:49 +1000

    Update script.js

--- script.js
+++ script.js
@@ -216,5 +216,5 @@ var exportresult = function()
 			species = showdownFormes[i][1]
 	}
 	}
-	document.getElementById("Output").innerHTML = monname "\ntesttesttest"
+	document.getElementById("Output").innerHTML = monname
 }
