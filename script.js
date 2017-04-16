8dded5e72f137b7e06789f37009b26271aa2f917
LegoFigure11 <legofigurefb@gmail.com>
2017-04-16 23:32:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -157,12 +157,13 @@ var savecustom = function()
 		
 
 			}
-			/*
-			var move1 = movarray.split(",")[0]
-			var move2 = movarray.split(",")[1]
-			var move3 = movarray.split(",")[2]
-			var move4 = movarray.split(",")[3]
-			*/
+			var movlist = movarray.toString()
+			
+			var move1 = movlist.split(",")[0]
+			var move2 = movlist.split(",")[1]
+			var move3 = movlist.split(",")[2]
+			var move4 = movlist.split(",")[3]
+			
 
 		}
 	}
@@ -232,7 +233,7 @@ var savecustom = function()
 
 
 		
-	var res = ln1p1.concat(species, ln1p2, ln2, ln3, ln4, ln5p1, ln5p2, lnbr, ln6p1, ln6p2, lnbr, ln7p1, ln7p2, lnbr, ln8p1, ln8p2, lnbr, ln9p1, ln9p2, lnbr, ln10p1, ln10p2, lnbr, ln11, ln12p1, ln5p1, ln12p2, lnbr, ln6p1, ln13p2, lnbr, ln7p1, ln14p2, lnbr, ln8p1, ln15p2, lnbr, ln9p1, ln16p2, lnbr, ln10p1, ln17p2, lnbr, ln11, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, movarray);
+	var res = ln1p1.concat(species, ln1p2, ln2, ln3, ln4, ln5p1, ln5p2, lnbr, ln6p1, ln6p2, lnbr, ln7p1, ln7p2, lnbr, ln8p1, ln8p2, lnbr, ln9p1, ln9p2, lnbr, ln10p1, ln10p2, lnbr, ln11, ln12p1, ln5p1, ln12p2, lnbr, ln6p1, ln13p2, lnbr, ln7p1, ln14p2, lnbr, ln8p1, ln15p2, lnbr, ln9p1, ln16p2, lnbr, ln10p1, ln17p2, lnbr, ln11, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, move1);
     document.getElementById("Output").innerHTML = res
 
 
