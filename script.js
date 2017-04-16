2fc6fdd4e72d3930803f82ffc6ffb912600fd4dd
LegoFigure11 <legofigurefb@gmail.com>
2017-04-16 22:38:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -140,6 +140,11 @@ var savecustom = function()
 				}
 
 			}
+			if(lines[i].indexOf("- ") != -1)
+			{
+				var move1 = lines[i]
+			}
+				    
 			if(lines[i].indexOf("Nature") != -1) //if nature is in this line
 			{
 				nature = lines[i].split(' ')[0].trim()
@@ -217,11 +222,7 @@ var savecustom = function()
 		var ln14p2 = IVs[2]
 		var ln15p2 = IVs[3]
 		var ln16p2 = IVs[4]
-		var ln17p2 = IVs[5]
-		var str = moves
-		var move1 = str.substring(0, str.indexOf(',')).trim
-
-	
+		var ln17p2 = IVs[5]	
 		
 	var res = ln1p1.concat(species, ln1p2, ln2, ln3, ln4, ln5p1, ln5p2, lnbr, ln6p1, ln6p2, lnbr, ln7p1, ln7p2, lnbr, ln8p1, ln8p2, lnbr, ln9p1, ln9p2, lnbr, ln10p1, ln10p2, lnbr, ln11, ln12p1, ln5p1, ln12p2, lnbr, ln6p1, ln13p2, lnbr, ln7p1, ln14p2, lnbr, ln8p1, ln15p2, lnbr, ln9p1, ln16p2, lnbr, ln10p1, ln17p2, lnbr, ln11, ln19p1, nature, lnen, ln20p1, ability, lnen, ln21p1, item, lnen, ln22, lnmv, move1, lnen, nextMove, lnen, moves);
     document.getElementById("Output").innerHTML = res
