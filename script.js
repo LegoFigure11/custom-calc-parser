5819dd718198da69344c9489174fe0a2c66be77d
LegoFigure11 <legofigurefb@gmail.com>
2017-08-03 06:13:42 +1000

    Flip nhb order

--- script.js
+++ script.js
@@ -157,18 +157,18 @@ var savecustom = function()
 		    if (lines[0].indexOf('(') != -1)
 		    {
 			    if (document.getElementById('l100').checked) {
-			var nhb = " - NO HOLDS BARRED"
+			var nhb = "NO HOLDS BARRED - "
 			var ln2p2_b = lines[0].substring(0, lines[0].indexOf('(')).trim()
-			var ln2p2 = ln2p2_b.concat(nhb)
+			var ln2p2 = nhb.concat(ln2p2_b)
 			} else {
 			var ln2p2 = lines[0].substring(0, lines[0].indexOf('(')).trim()
 			}
 		    }
 		    else {
 			    if (document.getElementById('l100').checked) {
-			var nhb = " - NO HOLDS BARRED"
+			var nhb = "NO HOLDS BARRED - "
 			var ln2p2_b = "set name"
-			var ln2p2 = ln2p2_b.concat(nhb)
+			var ln2p2 = nhb.concat(ln2p2_b)
 			} else {
 			var ln2p2 = "set name"
 			}
